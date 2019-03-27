15a17419becf58156a36526febe31bcac55dcb74
Eclipse <santiago.paiz@gmail.com>
2019-03-26 20:48:39 -0600

    Added Testing, and created first component with renderer

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
-/**
- * @format
- */
+import React from 'react';                  // Knows how a component should behave. Knows how to make a bunch of components
+// import ReactNative from 'react-native';  // Outputs component to the devices screen, provides default components (images, tables, text, etc)
+import { Text, AppRegistry } from 'react-native'         // import destructuring, for importing just specific atributes
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+// Create component
+// A component is a java script function that returns some ammount of JSX
+const App = () => (
+    // JSX is utilized to communicate to react native
+    // https://babeljs.io/repl es una pagina donde muestra como esta creando de JSX -> React de una manera mas eficiente
+    <Text>Some Text</Text>
+    );
+// Because its just returning a text we don't have to make the whole constructor
+/* const App = () => {
+    return (
+      <Text>Some Text</Text>
+    );
+}; */
 
-AppRegistry.registerComponent(appName, () => App);
+// Render to device
+AppRegistry.registerComponent('MedLine', () => App) // We have to register at least one component to the aplication
